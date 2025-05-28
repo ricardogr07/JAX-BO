541fa78a4e50390ae3ba811d555729d2f96cbb55
Ricardo García Ramírez <rgr.5882@gmail.com>
2025-05-28 00:30:44 -0600

    Update libraries

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ with io.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 INSTALL_REQUIRES = [
     'numpy',
     'scipy',
-    'jax>=0.4.23,<0.5.0',
-    'jaxlib>=0.4.23,<0.5.0',
+    'jax',
+    'jaxlib',
     'scikit-learn',
     'KDEpy',
     'pyDOE',
